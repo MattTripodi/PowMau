484db038eb1bc9c17165025d24bdff1bcfe3ba4f
Matt Tripodi <mtripodi1027@gmail.com>
2017-02-07 14:03:44 -0500

    Added Firebase to Pow Mau, Created PodFile

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+
+target 'PowMau' do
+ 
+  use_frameworks!
+pod 'Firebase/Core'
+ 
+end
---
+++ Pods/Target Support Files/Pods-PowMau/Pods-PowMau-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PowMau : NSObject
+@end
+@implementation PodsDummy_Pods_PowMau
+@end
